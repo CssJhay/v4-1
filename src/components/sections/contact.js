6585af4b5ad456350673a900e6d07078315cd4fc
CssJhay <84198099+CssJhay@users.noreply.github.com>
2023-03-20 13:44:08 +0100

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I am available for Freelance projects .I have a pre-vetted and handpicked programming talents 
+        for your contract engagaements. Hire me and get your projects done!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
