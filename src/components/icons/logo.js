88f2be992c86552d2c0ef68151b17c9ec455e7ef
CssJhay <84198099+CssJhay@users.noreply.github.com>
2023-03-26 00:34:11 +0100

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -15,14 +15,6 @@ c65 -69 248 -191 474 -316 94 -53 279 -160 410 -240 362 -219 386 -231 439
 -35 c50 -45 152 -94 196 -94 22 0 43 8 58 22 23 21 23 23 23 270 l0 248 -190
 0 -190 0 0 150 0 150 385 0 385 0 0 -412z"/>
 </g>
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
   </svg>
 );
 
