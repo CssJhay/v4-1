45e91b800db08f030df1dadce326a4e10f8b6604
CssJhay <84198099+CssJhay@users.noreply.github.com>
2023-04-08 22:07:26 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -108,9 +108,18 @@ const Footer = () => {
         <a href="https://github.com/cssjhay">
           <div>Designed &amp; Built by Joseph Olamide</div>
 
-        
+         {githubInfo.stars && githubInfo.forks && (
+            <div className="github-stats">
+              <span>
+                <Icon name="Star" />
+                <span>{githubInfo.stars.toLocaleString()}</span>
+              </span>
+              <span>
+                <Icon name="Fork" />
+                <span>{githubInfo.forks.toLocaleString()}</span>
+              </span>
             </div>
-       
+          )}
         </a>
       </StyledCredit>
     </StyledFooter>
